5dc61d47cdf332863c8088a8edc213e9c5140456
Kyle Garcia <kkpgarcia@gmail.com>
2021-06-18 22:12:21 +0800

    added shader abstractions

---
+++ src/engine/Renderer/Shader.ts
@@ -0,0 +1,85 @@
+import Debug from "../Debug/Debug";
+import RenderingContext from "./RenderingContext";
+
+export default class Shader
+{
+    private _shader: WebGLProgram;
+    private _uniformLocationCache: Map<string, WebGLUniformLocation>;
+
+    constructor(filePath?: string)
+    {
+        const vertCode = 'attribute vec2 coordinates;' + 'void main(void) {' + ' gl_Position = vec4(coordinates,0.0, 1.0);' + '}';
+        const fragCode = 'void main(void) {' + 'gl_FragColor = vec4(0.0, 0.0, 0.0, 0.1);' + '}';
+        this._shader = this.CreateProgram(vertCode, fragCode);
+    
+        this._uniformLocationCache = new Map<string, WebGLUniformLocation>();
+    }
+
+    private CreateProgram(vertexSource: string, fragmentSource: string): WebGLProgram
+    {
+        const gl = RenderingContext.instance.gl;
+        const program = gl.createProgram();
+
+        const vertShader = this.CreateShader(vertexSource, gl.VERTEX_SHADER);
+        const fragShader = this.CreateShader(fragmentSource, gl.FRAGMENT_SHADER);
+
+        gl.attachShader(program, vertShader);
+        gl.attachShader(program, fragShader);
+        gl.linkProgram(program);
+
+        gl.deleteShader(vertShader);
+        gl.deleteShader(fragShader);
+
+        return program;
+    }
+
+    private CreateShader(source: string, shaderType: number): WebGLShader
+    {
+        const gl = RenderingContext.instance.gl;
+        const shader = gl.createShader(shaderType);
+        gl.shaderSource(shader, source);
+        gl.compileShader(shader);
+        
+
+        return shader;
+    }
+
+    public Bind(): void
+    {
+        const gl = RenderingContext.instance.gl;
+        gl.useProgram(this._shader);
+    }
+
+    public Unbind(): void
+    {
+        const gl = RenderingContext.instance.gl;
+        gl.useProgram(0);
+    }
+
+    public SetUniform4f(name: string, data: number[]): void
+    {
+        const gl = RenderingContext.instance.gl;
+        gl.uniform4f(this.GetUniformLocation(name), data[0], data[1], data[2], data[3])
+    }
+
+    public GetUniformLocation(name: string): WebGLUniformLocation
+    {
+        const gl = RenderingContext.instance.gl;
+
+        if (this._uniformLocationCache.has(name))
+        {
+            return this._uniformLocationCache[name];
+        }
+
+        const location = gl.getUniformLocation(this._shader, name);
+        
+        if (!location)
+        {
+            Debug.Error("Uniform " + name + " doesn't exists");
+        }
+        
+        this._uniformLocationCache.set(name, location);
+
+        return location;
+    }
+}
--- src/webgl/Game.ts
+++ src/webgl/Game.ts
@@ -1,6 +1,8 @@
 import RenderingContext from "../engine/Renderer/RenderingContext";
 import { VertexBuffer, IndexBuffer, VertexBufferLayout } from "../engine/Renderer/Buffer";
 import VertexArray from "../engine/Renderer/VertexArray";
+import Shader from "../engine/Renderer/Shader";
+
 export default class Game
 {
     constructor(canvas: HTMLCanvasElement)
@@ -8,7 +10,6 @@ export default class Game
         RenderingContext.instance.Init(canvas);
         const gl = RenderingContext.instance.gl;
 
-        //Draw
         const vertices = [
             -0.5, 0.5,  // 0 
             -0.5, -0.5, // 1
@@ -16,43 +17,28 @@ export default class Game
             0.5, 0.5    // 3
         ];
 
-        const vertexBuffer = new VertexBuffer(vertices);
-        const vertexArray = new VertexArray();
-        const layout = new VertexBufferLayout();
-        layout.Push(2, gl.FLOAT);
-        vertexArray.AddBuffer(vertexBuffer, layout);
-
-        //Create a vertex shader
-        const vertCode = 'attribute vec2 coordinates;' + 'void main(void) {' + ' gl_Position = vec4(coordinates,0.0, 1.0);' + '}';
-        const vertShader = gl.createShader(gl.VERTEX_SHADER);
-        gl.shaderSource(vertShader, vertCode);
-        gl.compileShader(vertShader);
-
-        //create a fragment shader
-        const fragCode = 'void main(void) {' + 'gl_FragColor = vec4(0.0, 0.0, 0.0, 0.1);' + '}';
-        const fragShader = gl.createShader(gl.FRAGMENT_SHADER);
-        gl.shaderSource(fragShader, fragCode);
-        gl.compileShader(fragShader);
-
-        //Create a shader program
-        const shaderProgram = gl.createProgram();
-
-        //Attach Shaders
-        gl.attachShader(shaderProgram, vertShader);
-        gl.attachShader(shaderProgram, fragShader);
-
-        //Link program
-        gl.linkProgram(shaderProgram);
-
-        //use program
-        gl.useProgram(shaderProgram);
-
         const indices = [
             0, 1, 2, 
             0, 2, 3
         ];
 
+        const vertexBuffer = new VertexBuffer(vertices);
+        const vertexArray = new VertexArray();
+        const layout = new VertexBufferLayout();
+        
+        layout.Push(2, gl.FLOAT);
+        vertexArray.AddBuffer(vertexBuffer, layout);
+        
         const indexBuffer = new IndexBuffer(indices);
+
+        const shader = new Shader();
+
+
+        //Unbind all
+        vertexArray.Unbind();
+        shader.Unbind();
+        vertexBuffer.Unbind();
+        indexBuffer.Unbind();
 
         //Draw
         gl.clearColor(1, 1, 1, 1);
@@ -61,7 +47,9 @@ export default class Game
         gl.clear(gl.COLOR_BUFFER_BIT);
 
         gl.viewport(0, 0, canvas.width, canvas.height);
-
+        
+        shader.Bind();
+        
         vertexArray.Bind();
         indexBuffer.Bind();
 
